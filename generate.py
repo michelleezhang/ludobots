288465912f5fd856e1705f5af2e7b1f89a1255bd
michelleezhang <mhzhang1442@gmail.com>
2023-01-10 17:11:43 -0600

    multiple links

--- generate.py
+++ generate.py
@@ -4,10 +4,24 @@ import pyrosim.pyrosim as pyrosim
 # stores in world.sdf file
 
 # spcifies to pyrosim the file where world info is stored
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-# store a box with initial 3D posn and dimensions in box.sdf
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+x, y, z = 0, 0, 0.5
+
+for a in range(5):
+    for b in range(5):
+        length, width, height = 1, 1, 1
+
+        # store a box with initial 3D posn and dimensions in box.sdf
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+            length, width, height = length * 0.9, width * 0.9, height * 0.9
+            z += height
+        
+        x += 1
+        z = 0.5
+    x = 0
+    y += 1
 
 # close sdf file
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,13 +1,21 @@
 import pybullet as p
+import pybullet_data
 import time as time
 
 # This creates an object, physicsClient, which handles the physics, 
 # and draws the results to a Graphical User Interface (GUI).
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# add gravity
+p.setGravity(0,0,-9.8)
+
+# add a floor
+planeId = p.loadURDF("plane.urdf")
 
 # tells pybullet to read in the world described in box.sdf
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     # "steps" (moves forward) the physics inside the world for a small amount of time
