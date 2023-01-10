e8f687f7d76e8e4fac7d70b5bbbe2a6dd51c2af5
michelleezhang <mhzhang1442@gmail.com>
2023-01-10 15:32:44 -0600

    simulation

--- simulate.py
+++ simulate.py
@@ -1,1 +1,18 @@
-pass
+import pybullet as p
+import time as time
+
+# This creates an object, physicsClient, which handles the physics, 
+# and draws the results to a Graphical User Interface (GUI).
+
+physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    # "steps" (moves forward) the physics inside the world for a small amount of time
+    p.stepSimulation()
+
+    # sleep() suspends execution for the given number of seconds
+    time.sleep(1/60)
+
+    print(i)
+
+p.disconnect()
