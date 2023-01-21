d53780d56918ff20c1828256190cb81a5bfda6f4
michelleezhang <mhzhang1442@gmail.com>
2023-01-21 17:33:24 -0600

    synapses

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 import numpy as numpy
 pi_four = numpy.pi / 4.0
 
-num_iterations = 10000
+num_iterations = 1000
 
 max_force = 30
 
-time_step = 1/600 #60
+time_step = 1/240 #60
 
 # front_freq = 25
 # back_freq = 2
